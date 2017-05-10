ba8c93ef80ec7a68610e8842de47a471e7bf1048
david <david@192.168.0.15>
2017-05-09 20:24:53 -0700

    try it now

--- src/GUI/DrawingSurface.java
+++ src/GUI/DrawingSurface.java
@@ -28,11 +28,11 @@ public class DrawingSurface extends PApplet
 	// The statements in draw() are executed 60 times a second until the 
 	// program is stopped. Each statement is executed in 
 	// sequence and after the last line is read, the first 
-	// line is executed again.
+	// line is executed again.s
 
 
 	public void draw() 
-	{		
+	{		s
 		x =  player.getX();
 		y = player.getY();
 		
