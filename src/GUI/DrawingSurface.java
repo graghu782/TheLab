ff2ab2e7770bf213d7dee125d221c7e28fc947de
david <david@192.168.0.15>
2017-05-11 17:03:09 -0700

    Border works now

--- src/GUI/DrawingSurface.java
+++ src/GUI/DrawingSurface.java
@@ -50,8 +50,7 @@ public class DrawingSurface extends PApplet
 		background(255);
 		image(img, (float)(- x), (float)(-y), (float)(MAP_WIDTH+DRAWING_WIDTH), (float)(MAP_HEIGHT+DRAWING_HEIGHT));
 		
-		border = new Border(DRAWING_WIDTH/2-x-10-player.getWidth(),DRAWING_WIDTH/2-y-player.getLength(),MAP_WIDTH+20,MAP_HEIGHT+20);
-		this.rect((float)x, (float)y, 10, 10);
+		border = new Border(DRAWING_WIDTH/2-x-5-player.getWidth()/2,DRAWING_HEIGHT/2-y-5-player.getLength()/2,MAP_WIDTH+10,MAP_HEIGHT+10);
 	//	this.rect((float)(DRAWING_WIDTH/2), b, c, d);
 		border.draw(this);
 		
