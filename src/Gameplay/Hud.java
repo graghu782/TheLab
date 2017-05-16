2fbff2b8015ceebd4b2e0e357ddee80e731d549b
graghu782 <graghu@192.168.0.101>
2017-05-15 18:56:07 -0700

    hud stuff

--- src/Gameplay/Hud.java
+++ src/Gameplay/Hud.java
@@ -1,6 +1,25 @@
 package Gameplay;
 
+import processing.core.PApplet;
+
 public class Hud 
 { 
-
+    private static final double MAX_HEALTH = 100.0; 
+    
+    private double health;
+    private double ratio;
+    private String playerName;
+    
+    public Hud(String playerName) {
+	this.playerName = playerName;
+    }
+    
+    public void calculateRatio () {
+	ratio = health/MAX_HEALTH;
+    }
+    
+    public void draw(PApplet drawer, int x, int y) {
+	//display player name
+	//display rectangle which uses ratio to draw the health bar
+    }
 }
