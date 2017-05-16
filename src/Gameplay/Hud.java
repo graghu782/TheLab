a2a339de9eaa5477fc65aee806216064252ab1ee
kjust <kjust@DESKTOP-TCQC3RA>
2017-05-15 20:39:16 -0700

    Stuff

--- src/Gameplay/Hud.java
+++ src/Gameplay/Hud.java
@@ -2,24 +2,27 @@ package Gameplay;
 
 import processing.core.PApplet;
 
-public class Hud 
-{ 
-    private static final double MAX_HEALTH = 100.0; 
-    
+public class Hud
+{
+    private static final double MAX_HEALTH = 100.0;
+
     private double health;
     private double ratio;
     private String playerName;
-    
-    public Hud(String playerName) {
+
+    public Hud(String playerName)
+    {
 	this.playerName = playerName;
     }
-    
-    public void calculateRatio () {
-	ratio = health/MAX_HEALTH;
+
+    public void calculateRatio()
+    {
+	ratio = health / MAX_HEALTH;
     }
-    
-    public void draw(PApplet drawer, int x, int y) {
-	//display player name
-	//display rectangle which uses ratio to draw the health bar
+
+    public void draw(PApplet drawer, int x, int y)
+    {
+	// display player name
+	// display rectangle which uses ratio to draw the health bar
     }
 }
