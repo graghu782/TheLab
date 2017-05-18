685f0d1b9eb20cbb57bd37e7ff23268f51f440c9
graghu782 <graghu782@PR02.hhs.win.fuhsd.org>
2017-05-18 13:33:56 -0700

    Network !

--- src/Networking/Client.java
+++ src/Networking/Client.java
@@ -5,20 +5,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
+import java.net.UnknownHostException;
 
-public class Client {
-	public static void main(String[] args) {
-		String hostName = "172.16.240.166";
-		
-		try	 {
-			Socket s = new Socket(hostName, 4444);
-			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
-			pw.println("hello");
-			s.close();
-		} catch(IOException e) {
-			System.err.println(e);
-		}
+import GUI.DrawingSurface;
+
+public class Client implements Runnable
+{
+    private Socket socket;
+    private BufferedReader br;
+    private PrintWriter pw;
+    private Thread runningThread;
+    private boolean isStopped;
+    
+    public Client(String hostname, int port) throws UnknownHostException, IOException
+    {
+	socket = new Socket(hostname, port);
+
+    }
+
+    public static void main(String[] args)
+    {
+	String hostName = "localhost";
+
+	try
+	{
+	    Socket s = new Socket(hostName, 4444);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+	    PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
+	    pw.println("hello");
+	    s.close();
 	}
+	catch (IOException e)
+	{
+	    System.err.println(e);
+	}
+    }
+
+    public void run()
+    {
+	synchronized(this){
+	    this.runningThread = Thread.currentThread();
+	}
+
+	try
+	{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	    PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+	}
+	catch (IOException e)
+	{
+	    e.printStackTrace();
+	}
+	
+	while(!isStopped) {
+	    pw.println("playerpos");
+	    
+	}
+    }
 }
--- src/Networking/Server.java
+++ src/Networking/Server.java
@@ -2,23 +2,15 @@ package Networking;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Server implements Runnable {
-    //    	public Server(int port) {
-    //    	    
-    //    	}
-    //    
-    //	public void run()
-    //	{
-    //	    try {
-    //		ServerSocket ss = new ServerSocket(4444);
-    //		
-    //		while(true) {
+
     //			Socket connect = ss.accept();
     //			BufferedReader br = new BufferedReader(new InputStreamReader(connect.getInputStream()));
     //			PrintWriter pw = new PrintWriter(connect.getOutputStream(), true);
@@ -30,64 +22,65 @@ public class Server implements Runnable {
     //					break;
     //				}
     //			}
-    //		}
-    //		
-    //	} catch (IOException e) {
-    //		// TODO Auto-generated catch block
-    //		e.printStackTrace();
-    //	}
-    //	}
-    private int serverPort = 8080;
-    private ServerSocket serverSocket = null;
-    private boolean isStopped    = false;
+    private ServerSocket serverSocket;
+    private boolean isStopped;
     private Thread runningThread;
-
+    private int port;
+    
     public Server(int port){
-	this.serverPort = port;
+	this.port = port;
     }
 
     public void run(){
 	synchronized(this){
 	    this.runningThread = Thread.currentThread();
 	}
-	openServerSocket();
-	while(! isStopped()){	
+	
+	try
+	{
+	    serverSocket = new ServerSocket(port);
+	}
+	catch (IOException e)
+	{
+	    e.printStackTrace();
+	}
+	
+	while(!isStopped){	
 	    Socket clientSocket = null;
 	    try {
 		clientSocket = this.serverSocket.accept();
 	    } catch (IOException e) {
-		if(isStopped()) {
+		if(!isStopped) {
 		    System.out.println("Server Stopped.") ;
 		    return;
 		}
-		throw new RuntimeException(
-			"Error accepting client connection", e);
+		throw new RuntimeException("Error accepting client connection", e);
 	    }
-	    new Thread(
-		    new ServerThread(clientSocket, "Multithreaded Server")).start();
+
+	    try {
+		BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
+		while(true) {
+		    String in = br.readLine();
+		    if(in != null) {
+			System.out.println(in);
+			break;
+		    }
+		}
+	    } catch (IOException e) {
+	    }
 	}
+
 	System.out.println("Server Stopped.") ;
     }
 
-
-    private synchronized boolean isStopped() {
-	return this.isStopped;
-    }
-
     public synchronized void stop(){
-	this.isStopped = true;
+	isStopped = true;
 	try {
-	    this.serverSocket.close();
+	    serverSocket.close();
 	} catch (IOException e) {
 	    throw new RuntimeException("Error closing server", e);
 	}
     }
 
-    private void openServerSocket() {
-	try {
-	    this.serverSocket = new ServerSocket(this.serverPort);
-	} catch (IOException e) {
-	    throw new RuntimeException("Cannot open port 8080", e);
-	}
-    }
 }
